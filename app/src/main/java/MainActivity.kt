5a3a722dc1b426b981a275e1745fcf80fac7d5ad
Maickel Becker <maickelbecker@Maickels-Mac-mini.local>
2025-11-26 16:49:29 +0100

    Adiciona MainActivity e corrige erro de lint

---
+++ app/src/main/java/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.maickelbecker.educatransito
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface(color = MaterialTheme.colorScheme.background) {
+                Text("Bem-vindo ao EducaTransito!")
+            }
+        }
+    }
+}
